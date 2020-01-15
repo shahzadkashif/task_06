67812fafb0ddbd649ec44232427847f209bb598d
Kashif Shahzad <kashifshahzad@Kashifs-MacBook-Pro.local>
2020-01-15 18:48:44 +0300

    task6

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,3 +5,8 @@ class Restaurant(models.Model):
     description = models.TextField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+
+    def __str__(self):
+    	return self.name
+
+  
